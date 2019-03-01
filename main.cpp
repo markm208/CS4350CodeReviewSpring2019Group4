af7069b8fdb121e20638c495353540bc9f4accfb
Hoffman-Joshua <jhoffman5@carthage.edu>
2019-02-28 20:00:18 -0600

    Update main.cpp
    Added code to calculate the sum in fraction form

--- main.cpp
+++ main.cpp
@@ -11,7 +11,14 @@ int main()
 
 //new functions go here
 bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len){
+    
     convertToCmnDenom(n1, d1, n2, d2);
+
+    int cSum = c1 + c2; //add the integers together
+    int nSum = n1 + n2; //add the numerators together
+
+    cSum += nSum/d1; //add any overflow from the fraction
+    nSum -= nSum/d1; //subtract that overflow from the fraction
 
 }
 
