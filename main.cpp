14b464ebb257f2a84c6134bcd8378bd924ec0103
Hoffman-Joshua <jhoffman5@carthage.edu>
2019-02-28 19:49:05 -0600

    Update main.cpp
    Added a function to convert the fractions to a common denominator

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+void convertToCmnDenom(int& n1, int& d1, int& n2, int& d2);
+
 int main()
 {
     return 0;
@@ -9,5 +11,10 @@ int main()
 
 //new functions go here
 bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len){
-    
+    convertToCmnDenom(n1, d1, n2, d2);
+
+}
+
+void convertToCmnDenom(int& n1, int& d1, int& n2, int& d2){
+
 }
